d5538438e81c79916e8cdf006314cf465603865d
Manuel Silva <manuel@mjs.me>
2023-01-22 15:53:52 +0700

    Initial commit
    - Adds readme
    - Adds initial card layout
    - Some basic styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as unicodeEmoji from "unicode-emoji";
+import uniqid from "uniqid";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cards">
+        {unicodeEmoji
+          .getEmojisGroupedBy("subgroup")
+          ["food-prepared"].map((emojiObj) => {
+            return (
+              <div className="card" key={uniqid()}>
+                <div className="emoji">{emojiObj.emoji}</div>
+                <div className="description">{emojiObj.description}</div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
