fd074b6280689b0634c536b0fdcf45cc144a1945
Manuel Silva <manuel@mjs.me>
2023-01-22 16:38:31 +0700

    Add game logic. It's playable now.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import "./App.css";
 import * as unicodeEmoji from "unicode-emoji";
 import uniqid from "uniqid";
+import { useState } from "react";
+import _ from "lodash";
 
 const App = () => {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [memory, setMemory] = useState([]);
+  const [emojiList, setEmojiList] = useState(
+    unicodeEmoji.getEmojisGroupedBy("subgroup")["food-prepared"]
+  );
+
+  const gamePlay = (e, emoji) => {
+    // not selected yet
+    if (!memory.includes(emoji)) {
+      setMemory(memory.concat([emoji]));
+      setScore(score + 1);
+
+      //scramble emojiList
+      setEmojiList(_.shuffle(emojiList));
+    } else {
+      // already selected
+      // sets high-score
+      if (score > highScore) {
+        setHighScore(score);
+      }
+
+      // reset score
+      setScore(0);
+
+      // reset memory
+      setMemory([]);
+    }
+  };
+
   return (
     <div className="App">
       <header>
@@ -10,25 +42,29 @@ const App = () => {
         <div className="score">
           <div className="scoreContainer">
             <span className="label">SCORE</span>
-            <span className="score">0</span>
+            <span className="score">{score}</span>
           </div>
           <div className="scoreContainer">
             <span className="label">HIGH SCORE</span>
-            <span className="highScore">0</span>
+            <span className="highScore">{highScore}</span>
           </div>
         </div>
       </header>
       <div className="cards">
-        {unicodeEmoji
-          .getEmojisGroupedBy("subgroup")
-          ["food-prepared"].map((emojiObj) => {
-            return (
-              <div className="card" key={uniqid()}>
-                <div className="emoji">{emojiObj.emoji}</div>
-                <div className="description">{emojiObj.description}</div>
-              </div>
-            );
-          })}
+        {emojiList.map((emojiObj) => {
+          return (
+            <div
+              onClick={(e) => {
+                gamePlay(e, emojiObj.emoji);
+              }}
+              className="card"
+              key={uniqid()}
+            >
+              <div className="emoji">{emojiObj.emoji}</div>
+              <div className="description">{emojiObj.description}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
